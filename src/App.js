3ebfeec74610314f2a6610780ca658d40ddc5521
Lucas Reis <lucaskreis.com>
2022-05-11 19:54:03 -0300

    first front end commit

--- src/App.js
+++ src/App.js
@@ -1,18 +1,21 @@
 import { Routes, Route } from "react-router-dom";
-import { Home } from "./pages/Home";
 import { Login } from "./pages/Login";
 import { Signup } from "./pages/Signup";
 import { AuthContextComponent } from "./contexts/authContext";
 import { Profile } from "./pages/Profile";
+import { Edit } from "./pages/Edit";
+import { Trabalhos } from "./pages/Trabalhos";
+
 function App() {
   return (
     <>
       <AuthContextComponent>
         <Routes>
-          <Route path="/" element={<Home />} />
+          <Route path="/" element={<Login />} />
           <Route path="/signup" element={<Signup />} />
-          <Route path="/login" element={<Login />} />
           <Route path="/profile" element={<Profile />} />
+          <Route path="/Edit" element={<Edit/>} />
+          <Route path="/Trabalhos" element={<Trabalhos/>} />
         </Routes>
       </AuthContextComponent>
     </>
