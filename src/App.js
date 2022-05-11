3b5b34e5593bcde4cb96f8577fcd3f417b892944
Lucaskreis <101879209+Lucaskreis@users.noreply.github.com>
2022-05-11 18:32:30 -0300

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import { Routes, Route } from "react-router-dom";
+import { Home } from "./pages/Home";
+import { Login } from "./pages/Login";
+import { Signup } from "./pages/Signup";
+import { AuthContextComponent } from "./contexts/authContext";
+import { Profile } from "./pages/Profile";
+function App() {
+  return (
+    <>
+      <AuthContextComponent>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/signup" element={<Signup />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/profile" element={<Profile />} />
+        </Routes>
+      </AuthContextComponent>
+    </>
+  );
+}
+
+export default App;
